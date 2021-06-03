c76a412c6513682c7444a18b46a0b578a2029f43
onothoja marho <marho219@gmail.com>
2021-06-03 14:04:13 +0100

    palindrome algorithm

---
+++ sort_algorithms/mergesort.py
@@ -0,0 +1,54 @@
+# Python program for implementation of MergeSort
+def mergeSort(arr):
+    if len(arr) > 1:
+
+        mid = len(arr) // 2
+        L = arr[:mid]
+        R = arr[mid:]
+
+        mergeSort(L)
+        mergeSort(R)
+
+        i = j = k = 0
+
+        # Copy data to temp arrays L[] and R[]
+        while i < len(L) and j < len(R):
+            if L[i] < R[j]:
+                arr[k] = L[i]
+                i += 1
+            else:
+                arr[k] = R[j]
+                j += 1
+            k += 1
+
+        # Checking if any element was left
+        while i < len(L):
+            arr[k] = L[i]
+            i += 1
+            k += 1
+
+        while j < len(R):
+            arr[k] = R[j]
+            j += 1
+            k += 1
+
+        return arr
+
+
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    mid = len(arr) // 2
+
+    left = merge_sort(arr[:mid])
+    right = merge_sort(arr[mid:])
+
+    return left + right
+
+
+
+# Driver Code
+if __name__ == '__main__':
+    sortedlist = merge_sort([12, 11, 13, 5, 6, 7])
+    print(sortedlist)
